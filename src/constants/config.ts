e58d46e0e3c3307b2ddc9c0d46b7acf1146ed874
Hassaan Elahi <hassaan.elahi15@gmail.com>
2022-11-02 21:04:22 +0500

    added .env and changed config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,7 +1,6 @@
-export const ENVIRONMENT = process.env.REACT_APP_ELLIPSAS_ENV || 'development';
-export const GOOGLE_CLIENT_ID =
-    process.env.REACT_APP_GOOGLE_CLIENT_ID || '145824344638-kf025bde08sei4vkcljj6amie467uv8a.apps.googleusercontent.com';
+export const ENVIRONMENT: any = process.env.REACT_APP_ELLIPSAS_ENV;
+export const GOOGLE_CLIENT_ID: any = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-export const GOOGLE_CLIENT_SECRET = process.env.REACT_APP_GOOGLE_CLIENT_SECRET || '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';
+export const GOOGLE_CLIENT_SECRET = process.env.REACT_APP_GOOGLE_CLIENT_SECRET;
 
-export const API_BASE_URL = 'http://localhost:4000';
+export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
