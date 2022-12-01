ef2f96726dfc96177153764d6a954e386030505c
Hassaan Elahi <hassaan.elahi15@gmail.com>
2022-12-01 14:46:03 +0500

    updated react app .env

--- src/constants/network-calls.ts
+++ src/constants/network-calls.ts
@@ -1,4 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000/v1';
 export const AUTH_GOOGLE = '/auth/google';
 export const ORDERS = '/orders';
 export const ME = '/auth/me';
